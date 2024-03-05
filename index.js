3816b4ba833d87577db10682b0f829f493c20561
wendiemat <54000662+wendiemat@users.noreply.github.com>
2024-03-05 15:50:33 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// scroll reveal animation
+const scroll = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: false,
+});
+// home scroll
+scroll.reveal(".home-title", {});
+scroll.reveal(".home-scroll", { delay: 150 });
+// about scroll
+scroll.reveal(".about-img", { delay: 400 });
+scroll.reveal(".about-subtitle", { delay: 250 });
+scroll.reveal(".about-text", { delay: 300 });
+scroll.reveal(".about-link", { delay: 400 });
+
+// abilities scroll
+scroll.reveal(".skills-subtitle", {});
+scroll.reveal(".skills-name", { distance: "20px", delay: 50, interval: 100 });
+scroll.reveal(".skills-img", { delay: 300 });
+
+// work reveal
+scroll.reveal(".work-img", {});
+
+// contact scroll
+scroll.reveal(".contact-container", {});
+scroll.reveal(".social-container", { delay: 200 });
